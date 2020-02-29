57e895642c692f10733fbf3fc18062e6c452f9a9
User <User@192.168.0.11>
2020-02-29 19:24:25 -0300

    Atualizando Jenkinsfile e testrunner

--- src/test/java/testrunner/TestRunnerAll.java
+++ src/test/java/testrunner/TestRunnerAll.java
@@ -6,8 +6,9 @@ import cucumber.api.junit.Cucumber;
  
 @RunWith(Cucumber.class)
 @CucumberOptions(
-plugin = { "pretty", "json:src/test/java/Cucumber/cucumber-reports.json"},
-features="src/test/java/features",
+		
+plugin = {"pretty", "json:src/test/java/Cucumber/cucumber-reports.json"},
+features= "src/test/java/features",
 glue= "steps",
 dryRun = false)
 
